6cc219cdf2523597fda6e418df3b962d5daafa16
Harshit Raj Sinha <raj.harshitsinha08@gmail.com>
2026-06-01 14:16:36 +0000

    adding s3 integration

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -12,7 +12,6 @@ const Topbar = () => {
 	if (isLoading) {
 		return null; // or spinner
 	}
-	console.log("hererer", isLoading, isAuthenticated)
 	return (
 		<div
 			className='flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 
